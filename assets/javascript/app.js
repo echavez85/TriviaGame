64037100b93809614e38c8de32379d5397151dee
echavez85 <echavez85@gmail.com>
2018-10-31 14:31:15 -0500

    Removed questions from html and commented them into JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,4 +22,79 @@
 // After 10th question is answered or time runs out on 10th question, alert will display number of right, wrong, and unanswered questions and give player option to try again.
 
 
-
+{/* <div class="row">
+            <div class="col-md-12" id="quiz">
+                <form>
+                    <p>Which queen did a jumping death drop from a box during All Stars Season 3?</p>
+                    <input id="multiplechoice" type="radio" name="question1" value="Milk"> Milk<br>
+                    <input id="multiplechoice" type="radio" name="question1" value="Morgan"> Morgan McMichaels<br>
+                    <input id="multiplechoice" type="radio" name="question1" value="Aja"> Aja<br>
+                    <input id="multiplechoice" type="radio" name="question1" value="Trixie"> Trixie Mattel<br>
+                    <br>
+                    <br>
+                    <p>Which queen is known for popularizing the catchphrase "Not today, Satan"?</p>
+                    <input id="multiplechoice" type="radio" name="question2" value="Joslyn"> Joslyn Fox<br>
+                    <input id="multiplechoice" type="radio" name="question2" value="Darienne"> Darienne Lake<br>
+                    <input id="multiplechoice" type="radio" name="question2" value="Vivacious"> Vivacious<br>
+                    <input id="multiplechoice" type="radio" name="question2" value="Bianca"> Bianca del Rio<br>
+                    <br>
+                    <br>
+                    <p>Which queen is known for the catchphrase "Back Rolls?"?</p>
+                    <input id="multiplechoice" type="radio" name="question3" value="Coco"> Coco Montrese<br>
+                    <input id="multiplechoice" type="radio" name="question3" value="Adore"> Adore Delano<br>
+                    <input id="multiplechoice" type="radio" name="question3" value="Alyssa"> Alyssa Edwards<br>
+                    <input id="multiplechoice" type="radio" name="question3" value="Tatianna"> Tatianna<br>
+                    <br>
+                    <br>
+                    <p>Which queen eliminated Sasha Belle with kicks and splits during a lip sync for your life to Olivia Newton-John's "Twist of Fate"?</p>
+                    <input id="multiplechoice" type="radio" name="question4" value="Katya"> Katya<br>
+                    <input id="multiplechoice" type="radio" name="question4" value="Tempest"> Tempest Dujour<br>
+                    <input id="multiplechoice" type="radio" name="question4" value="Ginger"> Ginger Minj<br>
+                    <input id="multiplechoice" type="radio" name="question4" value="Kennedy"> Kennedy Davenport<br>
+                    <br>
+                    <br>
+                    <p>Which two queens returned after an iconic lip sync during All Stars Season 2?</p>
+                    <input id="multiplechoice" type="radio" name="question5" value="AlaskaDetox"> Alaska & Detox<br>
+                    <input id="multiplechoice" type="radio" name="question5" value="AlyssaTatianna"> Alyssa Edwards & Tatianna<br>
+                    <input id="multiplechoice" type="radio" name="question5" value="KatyaRoxxxy"> Katya & Roxxxy Andrews<br>
+                    <input id="multiplechoice" type="radio" name="question5" value="CocoPhiPhi"> Coco Montrese & PhiPhi O'Hara<br>
+                    <br>
+                    <br>
+                    <p>Which queen is known for her workroom entrance death drop?</p>
+                    <input id="multiplechoice" type="radio" name="question6" value="Shangela"> Shangela<br>
+                    <input id="multiplechoice" type="radio" name="question6" value="Laganja"> Laganja Estranja<br>
+                    <input id="multiplechoice" type="radio" name="question6" value="Magnolia"> Magnolia Crawford<br>
+                    <input id="multiplechoice" type="radio" name="question6" value="Gia"> Gia Gunn<br>
+                    <br>
+                    <br>
+                    <p>Which queen's jumping split fakeout had the judges rolling with laughter during Season 10?</p>
+                    <input id="multiplechoice" type="radio" name="question7" value="Monet"> Monet X Change<br>
+                    <input id="multiplechoice" type="radio" name="question7" value="Aquaria"> Aquaria<br>
+                    <input id="multiplechoice" type="radio" name="question7" value="Cracker"> Miz Cracker<br>
+                    <input id="multiplechoice" type="radio" name="question7" value="Kameron"> Kameron Michaels<br>
+                    <br>
+                    <br>
+                    <p>Which Season 5 queen snatched the judge's wigs with her own wig under a wig reveal?</p>
+                    <input id="multiplechoice" type="radio" name="question8" value="Detox"> Detox<br>
+                    <input id="multiplechoice" type="radio" name="question8" value="Jinkx"> Jinkx Monsoon<br>
+                    <input id="multiplechoice" type="radio" name="question8" value="Roxxxy"> Roxxxy Andrews<br>
+                    <input id="multiplechoice" type="radio" name="question8" value="Alaska"> Alaska<br>
+                    <br>
+                    <br>
+                    <p>Which Season 9 queen made jaws drop with her rose petals under wig reaveal during the Season 9 finale?</p>
+                    <input id="multiplechoice" type="radio" name="question9" value="Sasha"> Sasha Velour<br>
+                    <input id="multiplechoice" type="radio" name="question9" value="Shea"> Shea Coulee<br>
+                    <input id="multiplechoice" type="radio" name="question9" value="Trinity"> Trinity Taylor<br>
+                    <input id="multiplechoice" type="radio" name="question9" value="Peppermint"> Peppermint<br>
+                    <br>
+                    <br>
+                    <p>Which Season 7 queen inspired the All Stars Season 2 two-in-one look challenge?</p>
+                    <input id="multiplechoice" type="radio" name="question10" value="Fame"> Miss Fame<br>
+                    <input id="multiplechoice" type="radio" name="question10" value="Pearl"> Pearl<br>
+                    <input id="multiplechoice" type="radio" name="question10" value="Max"> Max<br>
+                    <input id="multiplechoice" type="radio" name="question10" value="Violiet"> Violet Chachki<br>
+                    <br>
+                    <br>
+                </form>
+            </div>
+        </div> */}
