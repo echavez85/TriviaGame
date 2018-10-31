3010cf2359166c499fa9bbe072c691da619cb918
echavez85 <echavez85@gmail.com>
2018-10-31 11:22:20 -0500

    Created pseudocode for JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// A "click to start" button will start the timer.
+
+
+// There will be an array with all ten questions and answers. Each question will identify which of the possible choices is the correct one.
+
+
+// The timer will be for 15 seconds for each question.
+
+
+// User will select one of four possible choices.
+
+
+// Function will log user choices.
+
+
+// "Submit" button will advance user to a gif displaying the correct answer. If a user does not select an answer within 15 seconds, game will advance to the gif automatically.
+
+
+// "Next Question" button below gif will advance user to next question in the questions array.
+
+
+// After 10th question is answered, alert will display number of right, wrong, and unanswered questions.
+
+
+
