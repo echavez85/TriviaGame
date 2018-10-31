67868a000b54e1a7be8926ace8274280258af710
echavez85 <echavez85@gmail.com>
2018-10-31 16:16:16 -0500

    Added var for q3 and q4 to JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,22 @@ var question02 = {
     question: "Which queen is known for popularizing the catchphrase 'Not today, Satan'?",
     answerChoices: ["Joslyn Fox", "Darienne Lake", "Vivacious", "Bianca del Rio"],
     values: ["incorrect", "incorrect", "incorrect", "correct"],
-    correct: "Aja",
+    correct: "Bianca del Rio",
     image: "./assets/images/biancanottoday.gif"
 };
 
+var question03 = {
+    question: "Which queen is known for the catchphrase 'Back Rolls?'?",
+    answerChoices: ["Coco Montrese", "Adore Delano", "Alyssa Edwards", "Tatianna"],
+    values: ["incorrect", "incorrect", "correct", "incorrect"],
+    correct: "Alyssa Edwards",
+    image: "./assets/images/alyssabackrolls.gif"
+};
+
+var question04 = {
+    question: "Which queen eliminated Sasha Belle with kicks and splits during a lip sync for your life to Olivia Newton-John's 'Twist of Fate'?",
+    answerChoices: ["Katya", "Tempest Dujour", "Ginger Minj", "Kennedy Davenport"],
+    values: ["correct", "incorrect", "incorrect", "incorrect"],
+    correct: "Katya",
+    image: "./assets/images/katyasplits.gif"
+};
