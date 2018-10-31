faac17fafea8cf667c2371f4a67357f0e6ffdd53
echavez85 <echavez85@gmail.com>
2018-10-31 16:07:28 -0500

    Declared global variables. Created question 1.

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Declare global variables
+var time = 15;
+var intervalId = "";
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+var question01 = {
+    question: "Which queen did a jumping death drop from a box during All Stars Season 3?",
+    answerChoices: ["Milk", "Morgan McMichaels", "Aja", "Trixie Mattel"],
+    values: ["incorrect", "incorrect", "correct", "incorrect"],
+    correct: "Aja",
+    image: "./assets/images/ajadeathdrop.gif"
+};
+
